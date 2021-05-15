bb2594d57b0aa96a717d1f8371d3991be9622522
Leo Huerta <leonel.huerta.retamal@gmail.com>
2021-05-15 17:26:07 -0400

    Add code for creating 'Simce 4B' table.

---
+++ panels/simce/4B/codes/main.R
@@ -0,0 +1,47 @@
+
+####################################### Setup #####################################################
+
+rm(list = ls())
+
+t0 <- Sys.time() 
+
+library(dplyr) 
+library(data.table) 
+
+####################################### Functions #################################################
+
+getDataFromFile <- function(file.name) {
+  df <- fread(file.name, encoding = "UTF-8") 
+  
+  # Fix for column "noaplica" (it is only in the new tables) 
+  if (! "noaplica" %in% names(df)) { df$noaplica <- 0 }
+
+  df <- df %>% 
+    select(agno, 
+           rbd, 
+           nalu_lect4b_rbd, 
+           nalu_mate4b_rbd,
+           prom_lect4b_rbd,
+           prom_mate4b_rbd, 
+           noaplica) %>% 
+    filter(noaplica == 0)
+  
+  return(df)
+}
+
+####################################### Get data ##################################################
+
+file.names <- list.files("data")
+file.content <- lapply(file.names, function(x) getDataFromFile(paste0("data/", x)))
+
+####################################### Main task #################################################
+
+df <- rbindlist(file.content) %>% 
+  arrange(agno, rbd) 
+
+####################################### Save output ###############################################
+
+fwrite(df, file = paste0("output/", format(Sys.time(), "%Y%m%d"), "_Simce4B.csv"), sep = "|")
+
+print(Sys.time() - t0) 
+
